44f314f989b828fff6e19aa424b60aac79ff42a6
Giacomo Tollardo <tollardogiacomo2@gmail.com>
2026-04-30 13:48:28 +0200

    Fixed error blockchain.h and .c

--- code/source/blockchain.c
+++ code/source/blockchain.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <inttypes.h>												// libreria per l'uso di "sprintf()"
 #include "blockchain.h"
 
 
-void int_to_hex(uint64_t value, char *out){
-	sprintf(out, "%016" PRIx64, value); 							//%016 aggiunge zeri iniziali per avere sempre 16 caratteri
+void int_to_hex(uint64_t value, char *hex){
+	snprintf(hex, 17, "%016lx", value); 							//%016 aggiunge zeri iniziali per avere sempre 16 caratteri
 }
 
 
-uint64_t hex_to_int(const char *value){
-
+uint64_t hex_to_int(const char *hex){
+	return (uint64_t)strtoull(hex, NULL, 16);
 }
